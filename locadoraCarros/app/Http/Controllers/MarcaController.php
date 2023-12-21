1a21ca4e437d682fcc82e7888145bb8cb1a9e611
Elton Andrade <eltonsa75@hotmail.com>
2023-12-21 13:59:18 -0300

    Validação no Update lidando com o PUT/PATCH

--- locadoraCarros/app/Http/Controllers/MarcaController.php
+++ locadoraCarros/app/Http/Controllers/MarcaController.php
@@ -59,16 +59,30 @@ class MarcaController extends Controller
     public function update(Request $request, $id)
     {
        $marca = $this->marca->find($id);
-       
+
        if($marca === null){
            return response()->json(['msg' => 'Impossível realizar a atualização. O recusro solicitado não existe'], 404);
        }
+
+         //se o método for PATCH, então é uma atualização parcial
+       if($request->method() === 'PATCH'){
+           $regrasDinamicas = array();
+
+           //percorrendo todas as regras definidas no Model
+           foreach($marca->rules() as $input => $regra){
+               //coletar apenas as regras aplicáveis aos parâmetros parciais da requisição PATCH
+               if(array_key_exists($input, $request->all())){
+                   $regrasDinamicas[$input] = $regra;
+               }
+           }
+           $request->validate($regrasDinamicas, $marca->feedback());
 
        $request->validate($marca->rules(), $marca->feedback());
 
        $marca->update($request->all());
        return  response()->json(['msg' => 'A marca foi atualizada com sucesso'], 200);
     }
+}
 
     /**
      * Remove the specified resource from storage.
